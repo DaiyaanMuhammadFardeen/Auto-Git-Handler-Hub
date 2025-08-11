ada3eae6d47d1f8632cd00269e95fdc0679eb896
Daiyaan Muhammad <daiyaan.muhammad2002@gmail.com>
2025-08-11 19:37:26 +0600

    A place for all the global variables. Will be used extensively later

---
+++ globals.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+declare -A MENU_MAP=(
+  [1]="View All Files"
+  [2]="Automation Menu"
+  [3]="Commit & Push"
+  [4]="Branch Management"
+  [5]="Settings"
+  [6]="Backup & Export"
+  [7]="Dev Tools"
+  [8]="Insights & Analysis"
+  [9]="Conflict Helper"
+  [10]="Exit"
+)
